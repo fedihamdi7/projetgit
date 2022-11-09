906b8794f0bffb6661f846fde6825eb179118f77
Fedi Hamdi <fedihamdi97@gmail.com>
2022-11-09 11:30:32 +0100

    first commit

---
+++ src/main/java/com/alinfo5/projetgit/ProjetgitApplication.java
@@ -0,0 +1,13 @@
+package com.alinfo5.projetgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alinfo5/projetgit/ProjetgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alinfo5.projetgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
