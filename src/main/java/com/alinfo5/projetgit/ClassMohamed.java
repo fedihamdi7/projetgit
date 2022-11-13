542fda402674af05733b21e6b36cb4066c692fa9
Mohamed.dallel <mohamedaziz.dallel@esprit.tn>
2022-11-13 22:55:07 +0100

    class mohamed modified

--- src/main/java/com/alinfo5/projetgit/ClassMohamed.java
+++ src/main/java/com/alinfo5/projetgit/ClassMohamed.java
@@ -12,6 +12,16 @@ public static void max (int a ,int b) {
 
 
 }
+
+  public static void factor(int number) {
+        System.out.print("Factors of " + number + " are: ");
+        for (int i = 1; i <= number; ++i) {
+          if (number % i == 0) {
+            System.out.print(i + " ");
+          }
+        }
+    }
+    
     public static void main(String args[])
     {
       max(3,5) ;
