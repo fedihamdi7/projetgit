495ed1e608672a27da04afc8f185f4aeb40b3fd4
ademhbibi <ademhbibi2018@gmail.com>
2022-11-13 22:16:02 +0100

    commitademcnflict

--- src/main/java/com/alinfo5/projetgit/ClassAdem.java
+++ src/main/java/com/alinfo5/projetgit/ClassAdem.java
@@ -1,14 +1,16 @@
 package com.alinfo5.projetgit;
 
 public class ClassAdem {
-
+        public static int Calculsoustraction(int a,int b){
+                return a-b;
+                    }
     public static Double CalculAvg (Double a,Double b){
             return (a+b)/2;
     }
     public static void main(String[] args) {
 		
         System.out.println(CalculAvg(8.5,9.5));
-
+        System.out.println(Calculsoustraction(6,5));
 
 	}
 }
