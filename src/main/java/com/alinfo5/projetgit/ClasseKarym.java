6c93055b3131b78f247f32f24d03bd3d77868358
karymgharby <karimgharbi0@gmail.com>
2022-11-13 22:49:06 +0100

    delete parite function

--- src/main/java/com/alinfo5/projetgit/ClasseKarym.java
+++ src/main/java/com/alinfo5/projetgit/ClasseKarym.java
@@ -1,19 +1,9 @@
 package com.alinfo5.projetgit;
 
 public class ClasseKarym {
-    public static void parite(Double a) {
-        if (a % 2 == 0) {
-            System.out.println(a + " est un nombre pair");
 
-        } else {
-            System.out.println(a + " est un nombre impair");
-
-        }
-        
-
-    }
-    public static Double produit (Double a, Double b){
-        return a*b;
+    public static Double produit(Double a, Double b) {
+        return a * b;
     }
 
     public static int Calculsoustraction(int a, int b) {
@@ -30,7 +20,6 @@ public class ClasseKarym {
     }
 
     public static void main(String[] args) {
-        parite(6.0);
         Calculsoustraction(5, 1);
     }
 
