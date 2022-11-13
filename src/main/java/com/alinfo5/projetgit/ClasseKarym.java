e28dad5c4bc7e1e605917bef79c48a08afbbb00e
karymgharby <karimgharbi0@gmail.com>
2022-11-13 22:52:11 +0100

    :sparkles: :cleanup code

--- src/main/java/com/alinfo5/projetgit/ClasseKarym.java
+++ src/main/java/com/alinfo5/projetgit/ClasseKarym.java
@@ -12,10 +12,6 @@ public class ClasseKarym {
 
     }
 
-    public static int Calculsoustraction(int a, int b) {
-        return a - b;
-    }
-
     public static void factor(int number) {
         System.out.print("Factors of " + number + " are: ");
         for (int i = 1; i <= number; ++i) {
@@ -27,7 +23,7 @@ public class ClasseKarym {
 
     public static void main(String[] args) {
         parite(6.0);
-        Calculsoustraction(5, 1);
+
     }
 
 }
