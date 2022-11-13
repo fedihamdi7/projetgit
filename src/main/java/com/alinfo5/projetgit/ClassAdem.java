d5771ea796c47684a7bde4e65ebf6fea9966af50
ahmedfourati54 <ahmedfourati54@gmail.com>
2022-11-13 22:19:35 +0100

    add parite

--- src/main/java/com/alinfo5/projetgit/ClassAdem.java
+++ src/main/java/com/alinfo5/projetgit/ClassAdem.java
@@ -5,10 +5,21 @@ public class ClassAdem {
     public static Double CalculAvg (Double a,Double b){
             return (a+b)/2;
     }
+    public static void parite(Double a) {
+        if (a % 2 == 0) {
+            System.out.println(a + " est un nombre pair");
+
+        } else {
+            System.out.println(a + " est un nombre impair");
+
+        }
+
+    }
+
     public static void main(String[] args) {
 		
         System.out.println(CalculAvg(8.5,9.5));
-
+        parite(6.0);
 
 	}
 }
