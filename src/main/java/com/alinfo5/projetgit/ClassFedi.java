463fac63260221ff36cdd103b3db7a09f9528a5f
Fedi Hamdi <fedihamdi97@gmail.com>
2022-11-13 21:55:11 +0100

    ajouter fonction parite

--- src/main/java/com/alinfo5/projetgit/ClassFedi.java
+++ src/main/java/com/alinfo5/projetgit/ClassFedi.java
@@ -9,6 +9,17 @@ public class ClassFedi {
           }
         }
     }
+
+    public static void parite(Double a) {
+      if (a % 2 == 0) {
+          System.out.println(a + " est un nombre pair");
+
+      } else {
+          System.out.println(a + " est un nombre impair");
+
+      }
+
+  }
     public static void main(String[] args) {
         ClassFedi.factor(12);
     }
