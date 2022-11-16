158e9e9b582e392ecda740f786df3d2a62a3cce9
Fedi Hamdi <fedihamdi97@gmail.com>
2022-11-16 22:51:03 +0100

    update class ahmed

--- src/main/java/com/alinfo5/projetgit/Classahmed.java
+++ src/main/java/com/alinfo5/projetgit/Classahmed.java
@@ -4,6 +4,16 @@ public class Classahmed {
     public static int Calculsoustraction(int a,int b){
 return a-b;
     }
+	public static void parite(Double a) {
+        if (a % 2 == 0) {
+            System.out.println(a + " est un nombre pair");
+
+        } else {
+            System.out.println(a + " est un nombre impair");
+
+        }
+
+    }
     
 	/**
 	 * @param args
