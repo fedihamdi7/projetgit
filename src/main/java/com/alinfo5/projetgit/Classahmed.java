285e228aac5b8f4f65ca9fba3a05ccb3a1508afd
karymgharby <karimgharbi0@gmail.com>
2022-11-16 22:48:54 +0100

    cleanup codes

--- src/main/java/com/alinfo5/projetgit/Classahmed.java
+++ src/main/java/com/alinfo5/projetgit/Classahmed.java
@@ -1,15 +1,27 @@
 package com.alinfo5.projetgit;
+
 public class Classahmed {
 
-    public static int Calculsoustraction(int a,int b){
-return a-b;
-    }
-    
+	public static int Calculsoustraction(int a, int b) {
+		return a - b;
+	}
+
+	public static void parite(Double a) {
+		if (a % 2 == 0) {
+			System.out.println(a + " est un nombre pair");
+
+		} else {
+			System.out.println(a + " est un nombre impair");
+
+		}
+
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(Calculsoustraction(6,5));
+		System.out.println(Calculsoustraction(6, 5));
 	}
 
 }
